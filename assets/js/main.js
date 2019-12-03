f8cdb55bc9066873a55dd684ebefe7a247922574
Milosz Bukala <mil.bukala@gmail.com>
2019-12-03 16:28:03 +0100

    Modyfikacje poludniowo-wtorkowe 03/12/2019r

--- assets/js/main.js
+++ assets/js/main.js
@@ -55,6 +55,29 @@ window.onload = function () {
     css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
     document.body.appendChild(css);
 };
+
+// BUTTON SCROLL TO TE TOP
+//Get the button:
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+
 // ---------------------------------------------------
 // -------- MAGIC STARS BACKGROUND EFFECT ------------
 // /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
