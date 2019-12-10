411eb9d16ca6017e90cc092be795c6145faa011d
Milosz Bukala <mil.bukala@gmail.com>
2019-12-10 01:53:38 +0100

    usprawnione html i js

--- assets/js/main.js
+++ assets/js/main.js
@@ -83,4 +83,19 @@ $(document).ready(function () {
         margin: 10
     });
 });
-// -------------------------------------------------------------------------------------
+
+// ------------------------ COUNTING TO THE NUMBER ---------------------------------
+$('#numbers .container .number .fuse h3').each(function () {
+    $(this).prop('Counter', 0).animate({
+        Counter: $(this).text()
+    }, {
+        duration: 4000,
+        easing: 'swing',
+        step: function (now) {
+            $(this).text(Math.ceil(now));
+        }
+    });
+});
+// --------------------------------------
+AOS.init();
+// --------------------------------------
